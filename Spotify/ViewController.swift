c9f585440018ab56d8aac76f65ebb92bc4361d07
Mateusz <mateusz.pusiewicz@ing.com>
2022-12-02 15:55:41 +0100

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Pusiewicz, M. (Mateusz) on 02/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
